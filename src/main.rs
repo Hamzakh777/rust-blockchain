6a181580c31e99e98e2ed2561da7fb626fc6eb0f
hamzakh777 <hamzabitup@gmail.com>
2021-04-26 00:01:03 +0000

    fix: set block hash

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use blockchainlib::*;
 
 fn main() {
-  let block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
-
+  let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
   println!("{:?}", &block);
-
+  
   let hash = block.hash();
-
   println!("{:?}", hash);
+  
+  block.hash = hash;
+  println!("{:?}", &block);
 }
